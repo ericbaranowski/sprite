4ccffe772f33c7c8cd5edd3548c1a4e5938ac949
Timo Staudinger <staudinger.timo@gmail.com>
2019-01-24 19:32:12 -0500

    Generate screenshots with svg content

--- packages/api/chart.ts
+++ packages/api/chart.ts
@@ -1,12 +1,26 @@
 import {IncomingMessage, ServerResponse} from 'http'
-import getScreenshot from './screenshot'
+import {parse} from 'url'
+import getScreenshot from './util/screenshot'
 
-const page = `
-<div style="background-color: red; padding: 50px;">Hello world</div>
-`
+const buildPage = (svg: string) =>
+  `<div id='target' style="background-color: red; padding: 50px;">${svg}</div>`
+
+const getCodeFromPath = (path: string) => {
+  let url = path.slice(7)
+  return url
+}
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-  const file = await getScreenshot(page)
+  const {pathname = '/', query = {}} = parse(req.url, true)
+  // const { type = 'png', quality, fullPage } = query;
+  const code = getCodeFromPath(pathname)
+  const decoded = Buffer.from(code, 'base64').toString()
+
+  console.log(decoded)
+
+  const page = buildPage(decoded)
+
+  const file = await getScreenshot(page, 'target')
   res.statusCode = 200
   res.setHeader('Content-Type', `image/png`)
   res.end(file)
