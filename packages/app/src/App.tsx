e8ffd4733e4febcd80023d76c243698ae21cf69d
Timo Staudinger <staudinger.timo@gmail.com>
2019-01-27 19:27:01 -0500

    Show warning on mobile browsers

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import SplitPane from 'react-split-pane'
+import MobileDetect from 'mobile-detect'
+
 import Editor from './Editor'
 import Preview from './Preview'
 import Header from './Header'
 import Share from './Share'
+import Help from './Help'
 
 import styles from './App.module.css'
-import Help from './Help'
 
 const App = () => {
   const [code, setCode] = useState(`graph TB
@@ -28,25 +30,50 @@ const App = () => {
   tooManyTries -- Yes --> finish
 `)
 
+  const isMobile = Boolean(
+    new MobileDetect(window.navigator.userAgent).mobile()
+  )
   const isPortrait = window.innerHeight > window.innerWidth
+
+  const [showMobileWarning, setShowMobileWarning] = useState(isMobile)
 
   return (
     <div className={styles.app}>
       <div className={styles.header}>
         <Header />
-        <Help />
-        {!isPortrait && <Share code={code} />}
+        {!isMobile && <Help />}
+        {!isMobile && <Share code={code} />}
       </div>
       <div className={styles.body}>
         <SplitPane
           split={isPortrait ? 'horizontal' : 'vertical'}
-          minSize={500}
           defaultSize="50%"
         >
           <Editor code={code} onChange={setCode} />
           <Preview code={code} />
         </SplitPane>
       </div>
+
+      {showMobileWarning && (
+        <div className={styles.dialog}>
+          <p>
+            <b>
+              Sprite doesn't properly support mobile browsers yet. Sorry for
+              that! 😔
+            </b>
+          </p>
+          <p>
+            It's on our radar though. If you want to follow along or contribute
+            yourself,{' '}
+            <a href="https://github.com/TimoSta/sprite/issues/10">
+              take a look at the issue tracker!
+            </a>
+          </p>
+          <button onClick={() => setShowMobileWarning(false)}>
+            Use Sprite anyway
+          </button>
+        </div>
+      )}
     </div>
   )
 }
